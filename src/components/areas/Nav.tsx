7c4d22c0d79da57c2345b9d01464858ebda392ee
Bak <95734631+BAKHONGMIN@users.noreply.github.com>
2023-10-11 13:32:41 +0900

    햄버거 아이콘 생성 및 팝업 생성
    npm i react-modal
    npm i @types/react-modal -D

--- src/components/areas/Nav.tsx
+++ src/components/areas/Nav.tsx
@@ -1,5 +1,54 @@
-import React from "react";
+import React, { useState } from "react";
+import { useWindowDimensions } from "../hooks/useWindowDimensions";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faBars } from "@fortawesome/free-solid-svg-icons";
+import "./Nav.css";
+import ReactModal from "react-modal";
+import SideBarMenus from "./sidebar/sidebarMenus";
 
 export default function Nav() {
-  return <nav className="navigation">Nav</nav>;
+  const { width } = useWindowDimensions();
+  const [showMenu, setShowMenu] = useState(false);
+
+  const getMobileMenu = () => {
+    if (width <= 768) {
+      return (
+        <FontAwesomeIcon
+          icon={faBars}
+          size="lg"
+          className="nav-mobile-menu"
+          onClick={onClickToggle}
+        />
+      );
+    }
+    return null;
+  };
+
+  const onClickToggle = (e: React.MouseEvent<Element, MouseEvent>) => {
+    setShowMenu(!showMenu);
+  };
+
+  const onRequestClose = (e: React.MouseEvent<Element, MouseEvent>) => {
+    setShowMenu(false);
+  };
+
+  return (
+    <>
+      <ReactModal
+        className="modal-menu"
+        // 여는지 닫는지 여부
+        isOpen={showMenu}
+        // 닫기 버튼을 누르면 동작
+        onRequestClose={onRequestClose}
+        // 화면 바깥을 누르면 닫는지 여부
+        shouldCloseOnOverlayClick={true}
+      >
+        <SideBarMenus />
+      </ReactModal>
+      <nav className="navigation">
+        {getMobileMenu()}
+        <strong>Super Forum</strong>
+      </nav>
+    </>
+  );
 }
